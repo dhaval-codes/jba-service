856592a090edd411f73835b977376303686b01f3
dhaval-codes <nimisha25dhaval@gmail.com>
2024-01-06 01:10:42 +0530

    installed more packages to make backend better

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+const port = 8080;
+const mongoURL = "mongodb://localhost:27017/appraisalDB"
+
+mongoose.connect(mongoURL)
+.then(()=>{
+    console.log("Successfully connected to MongoDB")
+})
+.catch((err)=>{
+    console.log("Error in connection: ", err)
+})
+
+app.listen(port,()=>{
+    console.log(`Backend is listening on ${port} successfully`)
+})
