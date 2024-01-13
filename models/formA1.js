ed11c4434478c6c841049d732a8a89661b1bc46a
dhaval-codes <nimisha25dhaval@gmail.com>
2024-01-14 03:02:24 +0530

    made schema for formA1type

---
+++ models/formA1.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+const {Schema} = mongoose;
+
+const formA1 = new Schema({
+    name: {type: String, required: true},
+    for: {type: String, required: true},
+    FormData: {type: Array, required: true}
+})
+
+export const FormA1 = mongoose.model("FormA1", formA1,"FormA1Data");
