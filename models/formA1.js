ac221218c0d8bda704e1dc8f875e569f62a2299f
dhaval-codes <nimisha25dhaval@gmail.com>
2024-02-01 13:27:16 +0530

    kuch to fix kiya hai

--- models/formA1.js
+++ models/formA1.js
@@ -4,7 +4,7 @@ const {Schema} = mongoose;
 const formA1 = new Schema({
     name: {type: String, required: true},
     for: {type: String, required: true},
-    FormData: {type: Array, required: true}
+    formData: {type: Array, required: true}
 })
 
 export const FormA1 = mongoose.model("FormA1", formA1,"FormA1Data");
--- routes/forms/submitForm.js
+++ routes/forms/submitForm.js
@@ -1,5 +1,10 @@
+import { FormA1 } from "../../models/formA1.js";
+import { FormC } from "../../models/formC.js";
+import { departmentWiseFormType } from "../../utils/department-formSelector.js";
+
 export const recieveFormData = async (req,res)=> {
     try{
+        // recieved all the necessary information
         const{
             formName,
             filledBy,
@@ -9,6 +14,14 @@ export const recieveFormData = async (req,res)=> {
             timePeriod,
             filledData
         } = req.body;
+        // console.log(req.body)
+        // processing filled data information to make Array of objects with questions and answers
+        if(formName === 'Appraisal Form A1') {
+            const fetchingDepartmentName = departmentWiseFormType(applicantsDepartment)
+            console.log(fetchingDepartmentName)
+            const completeFormData = await FormA1.findOne({for: fetchingDepartmentName})
+            console.log(completeFormData.formData);
+        }
             res.send('working')
     } catch (e) {
         console.log(e)
