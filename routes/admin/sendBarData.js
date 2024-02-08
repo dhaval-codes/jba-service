f5dbf6ea2b22384e7603e23dc307004aec4a5f8c
dhaval-codes <nimisha25dhaval@gmail.com>
2024-02-08 19:07:02 +0530

    added bar chart to admin's landing page

--- routes/admin/sendBarData.js
+++ routes/admin/sendBarData.js
@@ -1,85 +1,135 @@
+import { useState } from "react"
 import { FilledForm } from "../../models/filledForm.js"
 
 export const sendAdminBarChartData = async (req,res) =>{
     try{
-        const {send} = req.body
-        const role = send
-        if(role === 'Admin'){
-            let arrayOfAllForm = await FilledForm.find({name: 'Appraisal Form A1'})
-            const ScienceDept = [];
-            const CsDept = [];
-            const EngDept = [];
-            const PerArtsDept = [];
-            const HumanitiesDept = [];
-            const PeDept = [];
-            const ArtDept = [];
-            const PsyDept = [];
-            const CommerceDept = [];
-            const MathsDept = [];
-            const HindiDept = [];
-            const OtherDept = [];
-            arrayOfAllForm.map((item)=>{
-                switch (item.applicantsDepartment) {
-                    case 'science': ScienceDept.push(item);
-                    break;
+        const {role, dept, year} = req.body;
+        if (role === 'Admin') {
+            let Querry;
+            if(dept && year){
+                Querry = await FilledForm.find({$and:[
+                    {
+                        applicantsDepartment: dept
+                    },
+                    {
+                        "timePeriod.year": year
+                    }
+                ]});
+            } else if (year){
+                Querry = await FilledForm.find({"timePeriod.year": year})
+            } else if (dept){
+                Querry = await FilledForm.find({applicantsDepartment: dept})
+            } else {
+                Querry = await FilledForm.find()
+                let total = 0
+                Querry.map((item)=>{
+                    total = total + item.cumalativeMarks
+                })
+                const SchoolAvg = total/Querry.length
 
-                    case 'cs' : CsDept.push(item);
-                    break;
+                let engTotalMarks = 0;
+                let engCount = 0;
 
-                    case 'english': EngDept.push(item);
-                    break;
+                let hindiTotalMarks = 0;
+                let hindiCount = 0;
 
-                    case 'perArts': PerArtsDept.push(item);
-                    break;
+                let mathTotalMarks = 0;
+                let mathCount = 0;
 
-                    case 'humanities': HumanitiesDept.push(item);
-                    break;
+                let sciTotalMarks = 0;
+                let sciCount = 0;
 
-                    case 'pe': PeDept.push(item);
-                    break;
+                let csTotalMarks = 0;
+                let csCount = 0;
 
-                    case 'art': ArtDept.push(item);
-                    break;
+                let comTotalMarks = 0;
+                let comCount = 0;
 
-                    case 'psycology': PsyDept.push(item);
-                    break;
+                let humTotalMarks = 0;
+                let humCount = 0;
 
-                    case 'commerce': CommerceDept.push(item);
-                    break;
+                let psyTotalMarks = 0;
+                let psyCount = 0;
 
-                    case 'maths': MathsDept.push(item);
-                    break;
+                let peTotalMarks = 0;
+                let peCount = 0;
 
-                    case 'hindi': HindiDept.push(item);
-                    break;
+                let perArtTotalMarks = 0;
+                let perArtCount = 0;
 
-                    default: OtherDept.push(item);
+                let otherTotalMarks = 0;
+                let otherCount = 0;
+
+                Querry.forEach(item => {
+                    switch(item.applicantsDepartment){
+                        case "english":
+                            engTotalMarks += item.cumalativeMarks;
+                            engCount++
+                            break;
+                        case "hindi":
+                            hindiTotalMarks += item.cumalativeMarks;
+                            hindiCount++
+                            break;
+                        case "maths":
+                            mathTotalMarks += item.cumalativeMarks;
+                            mathCount++
+                            break;
+                        case "science":
+                            sciTotalMarks += item.cumalativeMarks;
+                            sciCount++
+                            break;
+                        case "cs":
+                            csTotalMarks += item.cumalativeMarks;
+                            csCount++
+                            break;
+                        case "commerce":
+                            comTotalMarks += item.cumalativeMarks;
+                            comCount++
+                            break;
+                        case "humanities":
+                            humTotalMarks += item.cumalativeMarks;
+                            humCount++
+                            break;
+                        case "psycology":
+                            psyTotalMarks += item.cumalativeMarks;
+                            psyCount++
+                            break;
+                        case "pe":
+                            peTotalMarks += item.cumalativeMarks;
+                            peCount++
+                            break;
+                        case "perArts":
+                            perArtTotalMarks += item.cumalativeMarks;
+                            perArtCount++
+                        default:
+                            otherTotalMarks += item.cumalativeMarks;
+                            otherCount++
+                    }
+                })
+
+                const EngAvg = engCount !== 0 ? (engTotalMarks / engCount).toFixed(2) : 0;
+                const HindiAvg = hindiCount !== 0 ? (hindiTotalMarks / hindiCount).toFixed(2) : 0;
+                const MathAvg = mathCount !== 0 ? (mathTotalMarks / mathCount).toFixed(2) : 0;
+                const SciAvg = sciCount !== 0 ? (sciTotalMarks / sciCount).toFixed(2) : 0;
+                const CsAvg = csCount !== 0 ? (csTotalMarks / csCount).toFixed(2) : 0;
+                const ComAvg = comCount !== 0 ? (comTotalMarks / comCount).toFixed(2) : 0;
+                const HumAvg = humCount !== 0 ? (humTotalMarks / humCount).toFixed(2) : 0;
+                const PsyAvg = psyCount !== 0 ? (psyTotalMarks / psyCount).toFixed(2) : 0;
+                const PeAvg = peCount !== 0 ? (peTotalMarks / peCount).toFixed(2) : 0;
+                const PerArtAvg = perArtCount !== 0 ? (perArtTotalMarks / perArtCount).toFixed(2) : 0;
+                const OtherAvg = otherCount !== 0 ? (otherTotalMarks / otherCount).toFixed(2) : 0;
+
+                const SendingObj = {
+                    schoolAvg: SchoolAvg.toFixed(2),
+                    subjCumalative: [EngAvg,HindiAvg,MathAvg,SciAvg,CsAvg,ComAvg,HumAvg,PsyAvg,PeAvg,PerArtAvg,OtherAvg]
                 }
-            })
-            const CumalativeArrray = [EngDept, HindiDept, MathsDept, ScienceDept, HumanitiesDept, CommerceDept, CsDept, PsyDept, ArtDept, PerArtsDept, OtherDept]
-            const Draft1 = [];
 
-            CumalativeArrray.forEach((department) => {
-                let departmentSum = 0;
-                let departmentName = '';
-
-                // Iterate over each object in the department array
-                department.forEach((obj) => {
-                    departmentName = obj.applicantsDepartment; // Assuming department name is the same for all objects in the array
-                    departmentSum += obj.filledData;
-                });
-
-                // Calculate the average filledData for the department
-                const departmentAverage = (departmentSum)*5;
-
-                // Push department name and average to Draft1 array
-                Draft1.push({ department: departmentName, averageFilledData: departmentAverage });
-            });
-
-            // console.log(Draft1)
-        } else (
-            res.send("Sorry you can't bypass this system")
-        )
+                res.send(SendingObj)
+            }
+            
+        } else {
+            res.send("Sorry you don't have access to this! Better luck next time")
+        }
     } catch (e) {
         console.log(e)
     }
